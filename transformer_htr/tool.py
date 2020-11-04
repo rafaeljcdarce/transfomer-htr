80c8cbddb8f614ebc9db25bd778051fa93e536da
Rafael <rd17647@bristol.ac.uk>
2020-11-04 14:55:04 +0000

    preprocess

--- transformer_htr/tool.py
+++ transformer_htr/tool.py
@@ -87,6 +87,42 @@ class Tokenizer():
             else:
                 string += char
         return string
+def augmentation(img,
+                 rotation_range=0,
+                 scale_range=0,
+                 height_shift_range=0,
+                 width_shift_range=0,
+                 dilate_range=1,
+                 erode_range=1):
+
+    img = img.astype(np.float32)
+    h, w = img.shape
+
+    dilate_kernel = np.ones((int(np.random.uniform(1, dilate_range)),), np.uint8)
+    erode_kernel = np.ones((int(np.random.uniform(1, erode_range)),), np.uint8)
+    height_shift = np.random.uniform(-height_shift_range, height_shift_range)
+    rotation = np.random.uniform(-rotation_range, rotation_range)
+    scale = np.random.uniform(1 - scale_range, 1)
+    width_shift = np.random.uniform(-width_shift_range, width_shift_range)
+
+    trans_map = np.float32([[1, 0, width_shift * w], [0, 1, height_shift * h]])
+    rot_map = cv2.getRotationMatrix2D((w // 2, h // 2), rotation, scale)
+
+    trans_map_aff = np.r_[trans_map, [[0, 0, 1]]]
+    rot_map_aff = np.r_[rot_map, [[0, 0, 1]]]
+    affine_mat = rot_map_aff.dot(trans_map_aff)[:2, :]
+
+    img = cv2.warpAffine(img, affine_mat, (w, h), flags=cv2.INTER_NEAREST, borderValue=255)
+    img = cv2.erode(img, erode_kernel, iterations=1)
+    img = cv2.dilate(img, dilate_kernel, iterations=1)
+
+    return img
+
+
+def normalization(img):
+    img = np.asarray(img).astype(np.float32)
+    img = img / 255
+    return img
 
 def preprocess_image(img):
 
